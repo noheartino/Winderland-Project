4df53799322996b9a0eb7486b36a71950ac6f609
roserosejump <forme1415@gmail.com>
2024-08-13 09:46:08 +0800

    修改app多餘import

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import "@/styles/a-style.sass";
+
 import { useEffect } from "react";
 
 
