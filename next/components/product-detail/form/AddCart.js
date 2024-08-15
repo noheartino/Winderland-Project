ecc5b41095a92afbb26529c4082c16a5a6833202
Shiori <nnn666nnn4125@gmail.com>
2024-08-15 15:05:26 +0800

    新增category圖片跟加入收藏按鈕互動效果

--- next/components/product-detail/form/AddCart.js
+++ next/components/product-detail/form/AddCart.js
@@ -1,11 +1,24 @@
-import React from "react";
-import styles from "./AddCart.module.css"
+import React,{useState,useEffect, use} from "react";
+import styles from "./AddCart.module.css";
 
 export default function AddCart() {
+
+  // 點擊收藏按鈕的效果 start
+
+  const [isFav,setIsFav] = useState(false); 
+  
+  const handleFavIcon = () => {
+    setIsFav(prevState => !prevState);
+  }
+
+  // 點擊收藏按鈕的效果 end
+
   return (
     <>
       <div className={`col-lg-1 col-md-1 col-1 ${styles["product-fav-icon"]}`}>
-        <i className="fa-regular fa-bookmark" />
+        <button type="button" className={`${styles['fav-button']}`} onClick={handleFavIcon}>
+          <i class={`fa-${isFav? 'solid' : 'regular'} fa-bookmark`}></i>
+        </button>
       </div>
       <div
         className={`col-lg-11 col-md-11 col-11 ${styles["product-add-cart"]}`}
