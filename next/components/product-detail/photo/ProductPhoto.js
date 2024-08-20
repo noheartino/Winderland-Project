a837fa71017cba11b5a99f9ec3fcc1998efc9e44
Shiori <nnn666nnn4125@gmail.com>
2024-08-20 10:58:37 +0800

    小香腸還沒改完先存檔

--- next/components/product-detail/photo/ProductPhoto.js
+++ next/components/product-detail/photo/ProductPhoto.js
@@ -1,7 +1,14 @@
 import React from "react";
 import styles from "./ProductPhoto.module.css";
+import { useProduct } from "@/context/ProductContext";
 
-export default function ProductPhoto({product}) {
+export default function ProductPhoto() {
+  const {product,loading,error} = useProduct();
+
+  if (loading) return <div>加載中...</div>;
+  if (error) return <div>{error}</div>;
+  if (!product) return <div>查無此商品</div>;
+
   return (
     <>
       {/* 商品大圖片 */}
@@ -17,23 +24,14 @@ export default function ProductPhoto({product}) {
         />
       </div>
       {/* 商品小圖片們 */}
-      <div className={`${styles['product-photos-sm']}`}>
-        <div className={`${styles['product-photo-sm']}`}>
-          <img 
-            className={`${styles['product-img']}`}
-            src="/product_images/8cacfd05b6d9923e4100abb79565d930 (1).png"
-            alt=""
-          />
-        </div>
-        <div className={`${styles['product-photo-sm']}`}>
-          <img
-          className={`${styles['product-img']}`}
-            src="/product_images/8cacfd05b6d9923e4100abb79565d930.png"
-            alt=""
-          />
-        </div>
-        <div className={`${styles['product-photo-sm']}`}></div>
-        <div className={`${styles['product-photo-sm']}`}></div>
+      <div className={`gap-2 ${styles['product-photos-sm']}`}>
+        {product[0].images.map((image,index) => (
+          <div key={index} className={`col-3 ${styles['product-photo-sm']}`}>
+            <img className={`${styles['product-img']}`} src={`/images/product/${image.path}`} alt="" />
+          </div>
+        ))}
+        <div className={`col-3`}></div>
+        <div className={`col-3`}></div>
       </div>
     </>
   );
