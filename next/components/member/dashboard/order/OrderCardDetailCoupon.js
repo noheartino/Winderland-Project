a781e028a7c3ca074d10799a25a62b7714b00111
roserosejump <forme1415@gmail.com>
2024-08-26 22:52:25 +0800

    [ 修正 ] 歷史訂單-未使用折價券邏輯

--- next/components/member/dashboard/order/OrderCardDetailCoupon.js
+++ next/components/member/dashboard/order/OrderCardDetailCoupon.js
@@ -4,36 +4,61 @@ import styles from '@/components/member/dashboard/order/OrderCardDetail.module.c
 
 
 export default function OrderCardDetailCoupon({ orderInfo }) {
+   // 有無使用折價券邏輯
+   const hasCoupon = orderInfo?.coupon_id && orderInfo?.coupon_name
+   // 有無使用 WPoints 邏輯
+   const hasUsedPoints = orderInfo?.pointUsed > 0
+
     return (
         <>
             <div className={`${styles.orderUsed} d-flex`}>
                 <h5 className={styles.orderUsedLabel}>本次使用</h5>
 
-                {orderInfo.coupon_name && (
-                <div className="used-coupon">
-                    <div
-                        className="coupon-card d-flex align-items-center"
-                        style={{ marginLeft: 98 }}
-                    >
-                        <div className={styles.couponTicket}>
-                            <div className="coupon-n">{orderInfo.coupon_name}</div>
+                 {/* 折價券區域 */}
+                 <div className={`${styles.usedItem} d-flex align-items-center`}>
+                    {hasCoupon ? (
+                        <div className="used-coupon">
+                            <div className="coupon-card d-flex align-items-center">
+                                <div className={styles.couponTicket}>
+                                    <div className="coupon-n">{orderInfo.coupon_name}</div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    ) : (
+                        <div className={styles.noCoupon}>
+                            本次未使用折價券
+                        </div>
+                    )}
                 </div>
-            )}
 
-            {orderInfo.pointUsed > 0 && (
-                <div className="used-wpoint">
-                    <div className="point d-flex">
-                        <div className={styles.pointImg} />
-                        <div className={styles.pointText}>
-                            <p>W Points</p>
-                            <div className={styles.pointUsed}>-{orderInfo.pointUsed}P</div>
+                {/* W Points 區域 */}
+                <div className={`${styles.usedItem} d-flex align-items-center`}>
+                    {hasUsedPoints ? (
+                        <div className="used-wpoint">
+                            <div className="point d-flex">
+                                <div className={styles.pointImg} />
+                                <div className={styles.pointText}>
+                                    <p>W Points</p>
+                                    <div className={styles.pointUsed}>-{orderInfo.pointUsed}P</div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    ) : (
+                        <div className={styles.noPoints}>
+                         
+                            <div className="point d-flex">
+                                <div className={styles.pointImg} />
+                                <div className={styles.pointText}>
+                                    <p>W Points</p>
+                                    <div className={styles.noPoints}>   
+                                    本次未使用 W Points</div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </div>
-)}
             </div>
+
             <div className={`${styles.orderGet} d-flex`}>
                 <h5 className={`${styles.orderGetLabel}`}>本次獲得</h5>
                 <div className="point d-flex">
