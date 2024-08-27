c34c720ccd97baa35219dfc0b8c5effc05f81303
roserosejump <forme1415@gmail.com>
2024-08-27 14:57:46 +0800

    [ 新增 ] profile會員卡串資料

--- next/components/member/dashboard/profile/ProfileMembership.js
+++ next/components/member/dashboard/profile/ProfileMembership.js
@@ -1,24 +1,69 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAuth } from '@/hooks/use-auth'
+import styles from '@/components/member/dashboard/profile/ProfileMembership.module.css'
 
 export default function ProfileMembership() {
+  const { auth } = useAuth();
+  const [membershipInfo, setMembershipInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchMembershipInfo = async () => {
+      try {
+        const response = await fetch(`http://localhost:3005/api/member/membership-info/${auth.userData.member_level_id}`, {
+          method: 'GET',
+          credentials: 'include',
+        });
+        const data = await response.json();
+
+        if (response.ok) {
+          setMembershipInfo(data.membershipInfo);
+        } else {
+          console.error('Failed to fetch membership info');
+        }
+      } catch (error) {
+        console.error('Error fetching membership info:', error);
+      }
+    };
+
+    if (auth.userData && auth.userData.member_level_id) {
+      fetchMembershipInfo();
+    }
+  }, [auth.userData]);
+
+  if (!membershipInfo) return null;
+
+  const levelColors = {
+    1: 'levelA',
+    2: 'levelB',
+    3: 'levelC',
+    4: 'levelD'
+  };
 
   return (
     <>
-      <div className="levelList ">
-                <div className="level">一般</div>
-                <div className="level levelB">白銀</div>
-                <div className="level levelC">黃金</div>
-                <div className="level levelD">白金</div>
-              </div>
-              <div className="levelDetail">
-                <span className="span-p">白金會員優惠</span>
-                <ul>
-                  <li>W Point 3.5倍回饋</li>
-                  <li>每月5張自選優惠券</li>
-                  <li>限量商品優先權</li>
-                  <li>3倍活動抽獎券</li>
-                </ul>
-              </div>
+      <div className={styles.levelList}>
+        <div className={`level ${auth.userData.member_level_id === 1 ? levelColors[1] : ''}`}>銅瓶</div>
+        <div className={`level ${auth.userData.member_level_id === 2 ? levelColors[2] : ''}`}>銀瓶</div>
+        <div className={`level ${auth.userData.member_level_id === 3 ? levelColors[3] : ''}`}>金瓶</div>
+        <div className={`level ${auth.userData.member_level_id === 4 ? levelColors[4] : ''}`}>白金瓶</div>
+      </div>
+
+      <div className="levelDetail">
+        <span className="span-p">{membershipInfo.name}會員優惠</span>
+
+        <ul className='levelGift'>
+          <li>生日回饋 
+            <span className={`${styles.emphasis} ${styles.birthdayPoints}`}>{membershipInfo.birthday_points}</span> WPoints
+          </li>
+          <li>WPoints 
+            <span className={`${styles.emphasis} ${styles.birthdayPoints}`}>{membershipInfo.points_reward_percentage}</span>倍回饋
+          </li>
+          <li>每月
+          <span className={`${styles.emphasis} ${styles.birthdayPoints}`}>{membershipInfo.free_coupon}</span>張自選優惠券</li>
+
+
+        </ul>
+      </div>
     </>
   )
 }
