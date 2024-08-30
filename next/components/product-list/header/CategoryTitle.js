e8889b696f0caf12301da500a756a2497d4448f0
Shiori <nnn666nnn4125@gmail.com>
2024-08-30 15:39:11 +0800

    增加背景圖片跟title的過渡特效

--- next/components/product-list/header/CategoryTitle.js
+++ next/components/product-list/header/CategoryTitle.js
@@ -1,31 +1,57 @@
-import React from "react";
-import styles from  "./CategoryTitle.module.css";
+import React, { useEffect, useState } from "react";
+import styles from "./CategoryTitle.module.css";
 
+export default function CategoryTitle({ filters, selectFilters }) {
+  const DEFAULT_IMAGE = "/shop_images/all.jpg";
+  const [currentImage, setCurrentImage] = useState(DEFAULT_IMAGE);
+  const [nextImage, setNextImage] = useState("");
+  const [isLoading , setIsLoading] = useState(true);
 
-export default function CategoryTitle({filters,selectFilters}) {
+  useEffect(() => {
+    setIsLoading(true);
+    let category = filters.categories.find(
+      (c) => c.id === selectFilters.category
+    );
+    const newImage = category
+      ? getImageUrl(category.img)
+      : getImageUrl("all.jpg");
 
-  let category = filters.categories.find((c) => {
-    if(c.id === selectFilters.category){
-      return c;
+    const img = new Image();
+    img.src = newImage;
+    img.onload=() => {
+      setNextImage(newImage);
+      setTimeout(() => {
+        setCurrentImage(newImage);
+        setIsLoading(false);
+      },300) //短暫延遲觸發過度效果
     }
-  })
+
+    img.onerror = () => {
+      console.error("Error loading image:",newImage);
+      setIsLoading(false);
+    }
+
+  }, [selectFilters.category, filters.categories]);
 
   // 取得圖片的路徑
   const getImageUrl = (filename) => {
-    if(!filename) return '';
+    if (!filename) return "";
     return `/shop_images/${filename}`;
-  }
+  };
 
-  const backgroundStyles = category ? 
-  {backgroundImage:`url(${getImageUrl(category.img)})`} : {backgroundImage:`url(${getImageUrl('all.jpg')})`}
+  let category = filters.categories.find(
+    (c) => c.id === selectFilters.category
+  );
 
   return (
     <>
-      <div className={styles["shop-header-img"]} style={backgroundStyles}>
+      <div className={`${styles["shop-header-img"]} ${isLoading ? styles["loading"] : ""}`}>
+        <div className={`${styles['background-layer']} ${styles['current']}`} style={{backgroundImage: `url(${currentImage})`}}></div>
+        <div className={`${styles['background-layer']} ${styles['next']}`} style={{backgroundImage: `url(${nextImage})`}}></div>
+        <div className={`${styles['overlay']}`}></div>
         <div className={styles["shop-header-title"]}>
-          {/* 到時候內容要跑資料庫的Category */}
-          <h1 className="h3">{category ? category.name : "全部商品"}</h1>
-          <h5>({category ? category.name_en : "All Wine"})</h5>
+          <h1 className={`h3 ${isLoading ? styles["loading"] : ""}`}>{category ? category.name : "全部商品"}</h1>
+          <h5 className={`${isLoading ? styles["loading"] : ""}`}>({category ? category.name_en : "All Wine"})</h5>
         </div>
       </div>
     </>
