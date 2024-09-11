801979869c46d94ae28ef355a79d778c2403e22f
CK.Liang <cody860314@gmail.com>
2024-09-11 17:51:43 +0800

    修正大小寫找不到路徑問題

--- next/components/product-detail/path/Path.js
+++ next/components/product-detail/path/Path.js
@@ -1,11 +1,11 @@
 import React from "react";
-import styles from "./path.module.css";
+import styles from "./Path.module.css";
 import { useProduct } from "@/context/ProductContext";
 import ClipLoader from "react-spinners/ClipLoader";
 
 
 export default function Path() {
-  const { product, loading, error} = useProduct();
+  const { product, loading, error } = useProduct();
 
   if (loading) {
     return (
@@ -29,21 +29,21 @@ export default function Path() {
 
   return (
     <>
-        <div className={`col ${styles['path']}` }>
-          <a className={`${styles['link']}`} href="/product">商品首頁</a>
-        </div>
-        <div className={`col ${styles['product-top-arrow']}`}>
-          <img className={`${styles['top-arrow']}`} src="/product_images/top-arrow.svg" alt="" />
-        </div>
-        <div className={`col ${styles['path']}`}>
-          <a className={`${styles['link']}`} href={`/product?category=${product[0].category_id}`}>{product[0].category_name}</a>
-        </div>
-        <div className={`col ${styles['product-top-arrow']}`}>
-          <img className={`${styles['top-arrow']}`} src="/product_images/top-arrow.svg" alt="" />
-        </div>
-        <div className={`col ${styles['path']}`}>
-          <a className={`${styles['link']}`} href={`/product?category=${product[0].category_id}&variet=${product[0].variet_name}`}>{product[0].variet_name}</a>
-        </div>
+      <div className={`col ${styles['path']}`}>
+        <a className={`${styles['link']}`} href="/product">商品首頁</a>
+      </div>
+      <div className={`col ${styles['product-top-arrow']}`}>
+        <img className={`${styles['top-arrow']}`} src="/product_images/top-arrow.svg" alt="" />
+      </div>
+      <div className={`col ${styles['path']}`}>
+        <a className={`${styles['link']}`} href={`/product?category=${product[0].category_id}`}>{product[0].category_name}</a>
+      </div>
+      <div className={`col ${styles['product-top-arrow']}`}>
+        <img className={`${styles['top-arrow']}`} src="/product_images/top-arrow.svg" alt="" />
+      </div>
+      <div className={`col ${styles['path']}`}>
+        <a className={`${styles['link']}`} href={`/product?category=${product[0].category_id}&variet=${product[0].variet_name}`}>{product[0].variet_name}</a>
+      </div>
     </>
   );
 }
